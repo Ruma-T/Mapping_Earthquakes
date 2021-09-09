ca7e40c390c5c82ccb709a99e5324cf64bd38e57
Ruma Talukder <sreeruta@yahoo.com>
2021-09-09 14:30:13 -0500

    Adding files with lines

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,7 +1,32 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+//let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+//let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790]
+//];
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+    color: "yellow"
+}).addTo(map);
+// Create a polyline using the line coordinates and make the line red.
+//L.polyline(line, {
+//   color: "red"
+//}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
@@ -9,7 +34,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -29,14 +54,14 @@ let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function (city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population / 100000
-    })
-        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-        .addTo(map);
-});
+//cityData.forEach(function (city) {
+// console.log(city)
+// L.circleMarker(city.location, {
+//    radius: city.population / 100000
+// })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+//});
 
 // We create the tile layer that will be the background of our map.
 // Then we add our 'graymap' tile layer to the map.
