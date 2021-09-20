e8f0d23898270f1be4a76380952e5957882ba184
Ruma Talukder <85513553+Ruma-T@users.noreply.github.com>
2021-09-19 21:17:22 -0500

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,1 @@
-onst API_KEY = "pk.eyJ1Ijoic3JlZXJ1dGEiLCJhIjoiY2t0OWpyc24zMWNxMDJ1cWtzbjBlMjF2aSJ9.15LDJ6lXtFMqFr3q3a2tgQ";
+const API_KEY = "pk.eyJ1Ijoic3JlZXJ1dGEiLCJhIjoiY2t0OWpyc24zMWNxMDJ1cWtzbjBlMjF2aSJ9.15LDJ6lXtFMqFr3q3a2tgQ";
